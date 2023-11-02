472c4f118e194259b6c9a158780cb5d0eceb4bd3
Doncey Albin <donceyalbin@gmail.com>
2023-11-02 10:02:31 -0600

    renamed

--- src/robotarm.c
+++ src/robotarm.c
@@ -1,3 +1,4 @@
+
 // Created by: Doncey Albin
 
 #ifdef __APPLE__
@@ -19,43 +20,97 @@
 
 // In-house includes
 //#include "plotter.h"
+#include "textureUtils.h"
+
+#include "simulateDrop.h"
+#include "simple_objects.h"
+#include "miscObjects.h"
+#include "groundPlane.h"
+#include "robot.h"
+
 #include "matrixMath.h"
 #include "forwardKinematics.h"
-#include "miscObjects.h"
-#include "textureUtils.h"
-#include "robot.h"
+
 #include "views.h"
 #include "lighting.h"
-#include "keyHandler.h" // Add this import last
+#include "keyHandler.h" // Add this import last  
+
+#include "CSCIx229.h"
+
+bool ballInHand() {
+    if (gripperClosed && fabsf(posX-endEffectorPosition.x)<0.1 &&  fabsf(posY-endEffectorPosition.y)<0.1 && fabsf(posZ-endEffectorPosition.z)<0.1) {
+        return true;
+        
+    }
+    else {
+        return false;
+    }
+}
 
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  displayView();            // View with respect to number key pressed
-  glPushMatrix();
-  glEnable(GL_DEPTH_TEST);  // Enable depth testing
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  setupLighting();          // Setup lighting (from lighting.h)
+    update();
+    displayView();
+    setupLighting();
 
-  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-  glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-  glEnable(GL_COLOR_MATERIAL);
+    glPushMatrix();
+    glEnable(GL_DEPTH_TEST);  // Enable depth testing
+    // Set up polygon mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+    
+    //GLfloat mat_ambient[] = {0.2, 0.2, 0.2, 1.0};  // Adjust this value as needed
+    //glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+    glRotatef((GLfloat)10.0, 1.0, 0.0, 0.0);
+    glTranslatef(0.0, 0.0, 0.0);                // Translate floor
+    drawGroundPlane();
+    glDisable(GL_DEPTH_TEST);
+    glPopMatrix();
 
-  GLfloat mat_ambient[] = {0.2, 0.2, 0.2, 1.0};  // Adjust this value as needed
-  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+    glPushMatrix();
+    glEnable(GL_DEPTH_TEST);  // Enable depth testing
+    // Set up polygon mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+    //GLfloat mat_ambient[] = {0.2, 0.2, 0.2, 1.0};  // Adjust this value as needed
+    //glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+    drawAxes(2.0);
+    drawRobotArm();
+    glDisable(GL_DEPTH_TEST);
+    glPopMatrix();
 
-  update();
-  // Axes
-  float axisLen = 0.5;
-  drawAxes(axisLen);
+    // Draw sphere simulation
+    glPushMatrix();
+    glEnable(GL_DEPTH_TEST);  // Enable depth testing
 
-  drawRobotArm();
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
 
-  glDisable(GL_DEPTH_TEST);  // Optionally disable depth testing if not needed later
+    bool ballInHandBool = ballInHand();
 
-  glPopMatrix();
+    if (ballInHandBool) {
+        posX = endEffectorPosition.x;
+        posY = endEffectorPosition.y;
+        posZ = endEffectorPosition.z;
+    }
+    else {
+        getPosition();
+    }
 
-  glFlush();
-  glutSwapBuffers();
+    glTranslatef(posX, posY, posZ);
+    Sphere(0.2, 100, 100);              // Draw a sphere
+
+    glDisable(GL_DEPTH_TEST);
+    glPopMatrix();
+
+    glutPostRedisplay();
+
+    glFlush();
+    glutSwapBuffers();
 }
 
 int main(int argc, char** argv) {
@@ -83,7 +138,7 @@ int main(int argc, char** argv) {
   //glutMotionFunc(mouseMove);
 
   // Init texture
-  loadTextureFromFile("./assets/cool.bmp");
+  //loadTextureFromFile("./assets/cool.bmp");
 
   //init();
   glutMainLoop();
