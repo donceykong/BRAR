290e1c0d493784bb3937e863a31e8f4d14a2f8a1
Doncey Albin <donceyalbin@gmail.com>
2023-12-11 01:22:12 -0700

    batch commit

---
+++ include/dummy.h
@@ -0,0 +1,6 @@
+#ifndef DUMMY_H
+#define DUMMY_H
+
+double getFiveTimes (double A, double B);
+
+#endif // DUMMY_H
---
+++ src/fiveTimes.c
@@ -0,0 +1,10 @@
+//  CSCIx229 library
+//  Willem A. (Vlakkies) Schreuder
+#include "dummy.h"
+
+//
+// Get Five Times
+//
+double getFiveTimes (double A, double B) {
+    return A * B;
+}
